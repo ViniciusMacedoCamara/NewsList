96eea3e5c718572d2063c8b82ab91a2928666f62
ViniciusMacedo <vini.macedo.c@gmail.com>
2021-03-28 12:44:13 -0300

    % amended sort by... | % passing actual data to details view

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:baking_news_list/models/tags.dart';
 import 'package:baking_news_list/services/webservice.dart';
 import 'package:baking_news_list/view/newsTile.dart';
 import 'package:basic_utils/basic_utils.dart';
@@ -46,12 +47,15 @@ class _HomePageState extends State<HomePage> {
   List<String> date = [];
   List<String> image = [];
   List<String> website = [];
-  // List<String> tags = [];
+  Tags tags;
   int sort = 0;
   List<int> test;
   bool fdac = false;
   News _news;
   List<News> novas = [];
+  List<String> enumValues = [];
+  final String whichSort = 'Sort by ';
+  bool isPassed = false;
 
   List news = [];
 
@@ -68,7 +72,83 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Text(widget.title),
         actions: <Widget>[
-          PopupOptionMenu(),
+          // IconButton(
+          //   icon: Icon(Icons.sort),
+          //   onPressed: () {
+          //     setState(() {
+          //       Comparator<News> datas = (a, b) => a.date.compareTo(b.date);
+          //       novas.sort(datas);
+          //     });
+          //
+          //     print(novas.elementAt(0).title);
+          //   },
+          // ),
+          // Sort by data
+          // Comparator<News> datas = (a, b) => a.date.compareTo(b.date);
+          // novas.sort(datas);
+
+          // Sort by title
+          // Comparator<News> titulos = (a, b) => a.title.compareTo(b.title);
+          // novas.sort(titulos);
+
+          // Sort by author
+          // Comparator<News> autores = (a, b) => a.authors.compareTo(b.authors);
+          // novas.sort(autores);
+          // PopupOptionMenu(),
+          PopupMenuButton<MenuOption>(
+              onSelected: (MenuOption result) {
+                print(result);
+                if (result.index == 0) {
+                  setState(() {
+                    // novas = [_news];
+                    Comparator<News> datas = (a, b) => a.date.compareTo(b.date);
+                    novas.sort(datas);
+                  });
+                } else if (result.index == 1) {
+                  setState(() {
+                    Comparator<News> titulos = (a, b) => a.title.compareTo(b.title);
+                    novas.sort(titulos);
+                  });
+                } else if (result.index == 2) {
+                  setState(() {
+                    Comparator<News> autores = (a, b) => a.authors.compareTo(b.authors);
+                    novas.sort(autores);
+                  });
+                } else {
+                  isPassed = false;
+                  setState(() {
+                    novas;
+                  });
+                }
+                print(result);
+                print(novas.toList().toString());
+                ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Sorted by ' + enumValues.elementAt(result.index))));
+              },
+              icon: Icon(Icons.sort),
+              itemBuilder: (BuildContext context) {
+                for (MenuOption menuOption in MenuOption.values) {
+                  enumValues.add(menuOption.toString().substring(menuOption.toString().indexOf('.') + 1));
+                }
+                return <PopupMenuEntry<MenuOption>>[
+                  PopupMenuItem(
+                    // child: Text('save'),
+                    child: Text(whichSort + enumValues.elementAt(0)),
+                    value: MenuOption.Date,
+                  ),
+                  PopupMenuItem(
+                    child: Text(whichSort + enumValues.elementAt(1)),
+                    value: MenuOption.Title,
+                  ),
+                  PopupMenuItem(
+                    child: Text(whichSort + enumValues.elementAt(2)),
+                    value: MenuOption.Author,
+                  ),
+                  PopupMenuItem(
+                    child: Text(whichSort + enumValues.elementAt(3)),
+                    value: MenuOption.Original,
+                  ),
+                ];
+              }),
         ],
       ),
       body: SafeArea(
@@ -77,35 +157,42 @@ class _HomePageState extends State<HomePage> {
             future: futureNews,
             builder: (BuildContext context, AsyncSnapshot snapshot) {
               if (snapshot.connectionState == ConnectionState.done && snapshot.hasData) {
-                for (var i = 0; i < snapshot.data.length; i++) {
-                  title.add(snapshot.data[i].title);
-                  authors.add(snapshot.data[i].authors);
-                  content.add(snapshot.data[i].content);
-                  date.add(snapshot.data[i].date);
-                  image.add(snapshot.data[i].imageUrl);
-                  website.add(snapshot.data[i].website);
+                if (isPassed == false) {
+                  novas = [];
+                  for (var i = 0; i < snapshot.data.length; i++) {
+                    // title.add(snapshot.data[i].title);
+                    // authors.add(snapshot.data[i].authors);
+                    // content.add(snapshot.data[i].content);
+                    // date.add(snapshot.data[i].date);
+                    // image.add(snapshot.data[i].imageUrl);
+                    // website.add(snapshot.data[i].website);
 
-                  _news = new News(
+                    _news = new News(
                       title: snapshot.data[i].title,
                       authors: snapshot.data[i].authors,
                       content: snapshot.data[i].content,
                       date: snapshot.data[i].date,
                       imageUrl: snapshot.data[i].imageUrl,
-                      website: snapshot.data[i].website);
+                      website: snapshot.data[i].website,
+                      tags: snapshot.data[i].tags,
+                    );
 
-                  novas.add(_news);
+                    novas.add(_news);
 
-                  // print(_news.title);
-                  // var newsInfo = {
-                  //   'title': snapshot.data[i].title,
-                  //   'authors': snapshot.data[i].authors,
-                  //   'content': snapshot.data[i].content,
-                  //   'date': snapshot.data[i].date,
-                  //   'image': snapshot.data[i].imageUrl,
-                  //   'website': snapshot.data[i].website
-                  // };
-                  // news.add(newsInfo);
+                    // print(_news.title);
+                    // var newsInfo = {
+                    //   'title': snapshot.data[i].title,
+                    //   'authors': snapshot.data[i].authors,
+                    //   'content': snapshot.data[i].content,
+                    //   'date': snapshot.data[i].date,
+                    //   'image': snapshot.data[i].imageUrl,
+                    //   'website': snapshot.data[i].website
+                    // };
+                    // news.add(newsInfo);
+                  }
+                  isPassed = true;
                 }
+                print(novas.elementAt(0).tags.elementAt(0).label);
 
                 // print(date);
                 // date.sort();
@@ -126,22 +213,22 @@ class _HomePageState extends State<HomePage> {
                           Text(novas.elementAt(index).date),
                         ],
                       ),
-                      // onTap: () {
-                      //   Navigator.push(
-                      //     context,
-                      //     MaterialPageRoute(
-                      //       builder: (context) => Detail(
-                      //         title: snapshot.data[index].title,
-                      //         author: snapshot.data[index].authors,
-                      //         content: snapshot.data[index].content,
-                      //         date: snapshot.data[index].date,
-                      //         image: StringUtils.addCharAtPosition(snapshot.data[index].imageUrl, 's', 4),
-                      //         website: snapshot.data[index].website,
-                      //         tags: snapshot.data[0].tags[0],
-                      //       ),
-                      //     ),
-                      //   );
-                      // },
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => Detail(
+                              title: novas.elementAt(index).title,
+                              author: novas.elementAt(index).authors,
+                              content: novas.elementAt(index).content,
+                              date: novas.elementAt(index).date,
+                              image: StringUtils.addCharAtPosition(novas.elementAt(index).imageUrl, 's', 4),
+                              website: novas.elementAt(index).website,
+                              tags: novas.elementAt(index).tags.elementAt(0),
+                            ),
+                          ),
+                        );
+                      },
                     );
                   },
                   separatorBuilder: (BuildContext context, int index) {
@@ -163,35 +250,44 @@ class _HomePageState extends State<HomePage> {
   }
 }
 
-enum MenuOption { Save, Discard, Draft }
+// testSort() {
+//   setState(() {
+//     Comparator<News> datas = (a, b) => a.date.compareTo(b.date);
+//     novas.sort(datas);
+//   });
+// }
 
-class PopupOptionMenu extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return PopupMenuButton<MenuOption>(
-        onSelected: (MenuOption result) {
-          print(result);
-        },
-        icon: Icon(Icons.sort),
-        itemBuilder: (BuildContext context) {
-          return <PopupMenuEntry<MenuOption>>[
-            PopupMenuItem(
-              // child: Text('save'),
-              child: Icon(
-                Icons.sort,
-                color: Colors.black87,
-              ),
-              value: MenuOption.Save,
-            ),
-            PopupMenuItem(
-              child: Text('Discard'),
-              value: MenuOption.Discard,
-            ),
-            PopupMenuItem(
-              child: Text('Draft'),
-              value: MenuOption.Draft,
-            ),
-          ];
-        });
-  }
-}
+enum MenuOption { Date, Title, Author, Original }
+
+// class PopupOptionMenu extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return PopupMenuButton<MenuOption>(
+//         onSelected: (MenuOption result) {
+//           if (result.index == 0) {
+//             print(result);
+//           }
+//         },
+//         icon: Icon(Icons.sort),
+//         itemBuilder: (BuildContext context) {
+//           return <PopupMenuEntry<MenuOption>>[
+//             PopupMenuItem(
+//               // child: Text('save'),
+//               child: Icon(
+//                 Icons.sort,
+//                 color: Colors.black87,
+//               ),
+//               value: MenuOption.Save,
+//             ),
+//             PopupMenuItem(
+//               child: Text('Discard'),
+//               value: MenuOption.Discard,
+//             ),
+//             PopupMenuItem(
+//               child: Text('Draft'),
+//               value: MenuOption.Draft,
+//             ),
+//           ];
+//         });
+//   }
+// }
